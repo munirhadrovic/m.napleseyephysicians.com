fe3ef9708cae787aa31144278ec10e8087a0702c
neswill1 <nenadkonatar@live.ac.me>
2015-03-26 09:36:58 +0100

    ispravka u hederu

--- first/index.php
+++ first/index.php
@@ -115,7 +115,8 @@
 		    <h1><img src="img/logo.gif" alt="LOGO"></h1>
 		</div>
 		<div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
-		    <h1>Mobile Site<a href="http://napleseyephysicians.com/" rel="external"> <span> Clasic Site</span></a></h1>
+
+            <h1 style="color: black;">Powered by <a href="http://mobilegrows.com/">MobileGrows </a>|<a href="http://napleseyephysicians.com/" rel="external"> Desktop Site</a></h1>
 		</div>
 		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
 		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
